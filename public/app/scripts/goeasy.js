0a9d3f9c912fc6e0dfcdd0b353af5db2f23b9b3a
Eric P <pcderic@gmail.com>
2019-06-13 02:40:14 -0400

    Reformat presentation. Support custom entry.

--- public/app/scripts/goeasy.js
+++ public/app/scripts/goeasy.js
@@ -12,6 +12,7 @@ function takeUnits(unitsTaken) {
     unitsLeft -= unitsTaken;
     $("#unitsLeft").val(unitsLeft);
     localStorage.setItem("unitsLeft", unitsLeft);
+    saveCustoms();
     return unitsLeft;
 }
 
@@ -19,4 +20,23 @@ function restoreUnits() {
     let unitsLeft = localStorage.getItem("unitsLeft");
     $("#unitsLeft").val(unitsLeft);
     return unitsLeft;
+}
+
+function saveCustoms() {
+    localStorage.setItem("inVolume", $("#inVolume").val());
+    localStorage.setItem("inStrength", $("#inStrength").val());
+}
+
+function restoreCustoms() {
+    $("#inVolume").val(localStorage.getItem("inVolume"));
+    $("#inStrength").val(localStorage.getItem("inStrength"));
+    $("#customUnits").val(getCustomUnits());
+}
+
+function getCustomUnits() {
+    let inStrength = $("#inStrength").val();
+    let inVolume = $("#inVolume").val();
+    let customUnits = Number((inStrength * inVolume) / 1000).toPrecision(2);
+    $("#customUnits").val(customUnits);
+    return customUnits;
 }
